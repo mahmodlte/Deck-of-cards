de132decafa0bb2d7e3cb314e7446eaa7576109f
Mahmoud Mansour <mahmodlte@gmail.com>
2021-07-14 09:09:58 +0300

    converted api calls functions to async functions

--- src/api/API.js
+++ src/api/API.js
@@ -3,19 +3,22 @@ const NEW_DECK = BASE_URL + "/new/shuffle/?deck_count=1";
 
 const fetchData = (...args) => fetch(...args).then((res) => res.json());
 let deckId = null;
-export const deckInit = () => {
-  return fetchData(NEW_DECK).then((res) => {
+export const deckInit = async () => {
+  try {
+    const res = await fetchData(NEW_DECK);
     deckId = res.deck_id;
     return deckId;
-  });
+  } catch (err) {
+    console.log(err);
+  }
 };
-export const deckDraw = (count = 2) => {
-  return fetchData(BASE_URL + `/${deckId}/draw/?count=${count}`).then(
-    (res) => res
-  );
+export const deckDraw = async (count = 2) => {
+  const res = await fetchData(BASE_URL + `/${deckId}/draw/?count=${count}`);
+  return res;
 };
-export const piles = (pileName, card1, card2) => {
-  return fetchData(
+export const piles = async (pileName, card1, card2) => {
+  const res = await fetchData(
     BASE_URL + `/${deckId}/pile/${pileName}/add/?cards=${card1},${card2}`
-  ).then((res) => res);
+  );
+  return res;
 };
